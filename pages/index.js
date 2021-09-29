37dbdde2f9d49ee7d33b63d51d090c28b33889ee
eli-front <elifront23@gmail.com>
2021-09-28 21:59:35 -0500

    schedules

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,14 @@ export default function Home({ dayType }) {
 	return (
 		<Layout>
 			<h1 className="text-4xl font-medium font-serif mt-5">
-				day type: {dayType}
+				Day Type: {dayType}
 			</h1>
 		</Layout>
 	);
 }
 
 export async function getStaticProps({ params }) {
-	let dayType;
-	await fetch(
+	const dayType = await fetch(
 		'https://calendar.google.com/calendar/ical/daily.schedule@jburroughs.org/public/basic.ics'
 	)
 		.then(response => {
@@ -22,7 +21,7 @@ export async function getStaticProps({ params }) {
 		.then(data => {
 			const cal = new Calendar(data);
 
-			dayType = cal.getDayType(new Date(2021, 8, 28));
+			return cal.getDayType(new Date(2021, 8, 28));
 		});
 	return {
 		props: {
--- pages/schedules.js
+++ pages/schedules.js
@@ -1,9 +1,59 @@
 import Layout from '../components/Layout.js';
+import Schedule from '../lib/schedule.js';
 
 export default function Schedules() {
+	const s = new Schedule();
+
+	const ref = {
+		'Regular Schedule': s.Reg,
+		'Late Start Day': s.Late,
+		'Special Schedule': s.Special,
+		'Common Day': s.Common
+	};
+
+	function periodLabel(index, isCom) {
+		if (index < 4 || !isCom) {
+			return index == 0 ? 'Assembly' : `Period ${index}`;
+		} else if (index == 4) {
+			return 'Common';
+		} else {
+			return `Period ${index - 1}`;
+		}
+	}
+
 	return (
 		<Layout>
 			<h1>schedules</h1>
+			{Object.entries(ref).map((item, i) => {
+				return (
+					<section key={i}>
+						<h3>{item[0]}</h3>
+						<table>
+							{item[1].map((period, j) => {
+								const isCom = item[1].length > 11;
+
+								return (
+									<tr key={j}>
+										<th>{periodLabel(j, isCom)}</th>
+										{period.map((_, k) => {
+											const h = Math.floor(
+												period[k] / 100
+											);
+											const m = period[k] % 100;
+											return (
+												<td key={j}>
+													{h > 12 ? h - 12 : h}:
+													{m < 10 ? `0${m}` : m}
+												</td>
+											);
+										})}
+									</tr>
+								);
+							})}
+						</table>
+					</section>
+				);
+			})}
 		</Layout>
 	);
 }

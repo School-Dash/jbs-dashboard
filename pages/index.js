024527b9d732d9429233ee572bcc7cfbf46e6d98
eli-front <elifront23@gmail.com>
2021-09-28 20:30:14 -0500

    tailwind setup

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,32 @@
-import styles from '../styles/Home.module.css';
 import Layout from '../components/Layout.js';
+import Calendar from '../lib/calendar.js';
 
-export default function Home() {
-	return <Layout></Layout>;
+export default function Home({ dayType }) {
+	return (
+		<Layout>
+			<h1 className="text-4xl font-medium font-serif mt-5">
+				day type: {dayType}
+			</h1>
+		</Layout>
+	);
+}
+
+export async function getStaticProps({ params }) {
+	let dayType;
+	await fetch(
+		'https://calendar.google.com/calendar/ical/daily.schedule@jburroughs.org/public/basic.ics'
+	)
+		.then(response => {
+			return response.text();
+		})
+		.then(data => {
+			const cal = new Calendar(data);
+
+			dayType = cal.getDayType(new Date(2021, 8, 28));
+		});
+	return {
+		props: {
+			dayType
+		}
+	};
 }

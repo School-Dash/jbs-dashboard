8d43f7bcb2c07097ca363bd5bb80ab073aa84ea5
Eli Front <elifront23@gmail.com>
2021-09-29 12:17:31 -0500

    schedule data organized

--- pages/schedules.js
+++ pages/schedules.js
@@ -1,16 +1,7 @@
 import Layout from '../components/Layout.js';
-import Schedule from '../lib/schedule.js';
+import { SCHEDULES, SCHEDULE_DAY_TYPES } from '../lib/schedule.js';
 
 export default function Schedules() {
-	const s = new Schedule();
-
-	const ref = {
-		'Regular Schedule': s.Reg,
-		'Late Start Day': s.Late,
-		'Special Schedule': s.Special,
-		'Common Day': s.Common
-	};
-
 	function periodLabel(index, isCom) {
 		if (index < 4 || !isCom) {
 			return index == 0 ? 'Assembly' : `Period ${index}`;
@@ -24,34 +15,36 @@ export default function Schedules() {
 	return (
 		<Layout>
 			<h1>schedules</h1>
-			{Object.entries(ref).map((item, i) => {
+			{Object.entries(SCHEDULES).map((item, i) => {
 				return (
-					<section key={i}>
-						<h3>{item[0]}</h3>
+					<div key={i}>
+						<h3>{SCHEDULE_DAY_TYPES[item[0]]}</h3>
 						<table>
-							{item[1].map((period, j) => {
-								const isCom = item[1].length > 11;
+							<tbody>
+								{item[1].map((period, j) => {
+									const isCom = item[1].length > 11;
 
-								return (
-									<tr key={j}>
-										<th>{periodLabel(j, isCom)}</th>
-										{period.map((_, k) => {
-											const h = Math.floor(
-												period[k] / 100
-											);
-											const m = period[k] % 100;
-											return (
-												<td key={j}>
-													{h > 12 ? h - 12 : h}:
-													{m < 10 ? `0${m}` : m}
-												</td>
-											);
-										})}
-									</tr>
-								);
-							})}
+									return (
+										<tr key={`${j} ${i}`}>
+											<th>{periodLabel(j, isCom)}</th>
+											{period.map((_, k) => {
+												const h = Math.floor(
+													period[k] / 100
+												);
+												const m = period[k] % 100;
+												return (
+													<td key={`${k} ${j} ${i}`}>
+														{h > 12 ? h - 12 : h}:
+														{m < 10 ? `0${m}` : m}
+													</td>
+												);
+											})}
+										</tr>
+									);
+								})}
+							</tbody>
 						</table>
-					</section>
+					</div>
 				);
 			})}
 		</Layout>

aa3a570f5044ff15220fdde5d56c98af6e58f7b8
eli-front <elifront23@gmail.com>
2021-09-28 20:05:04 -0500

    api

---
+++ pages/api/day-type.js
@@ -0,0 +1,32 @@
+import Calendar from '../../lib/calendar.js';
+
+export default async function handler(req, res) {
+	if (
+		req.query.year == null ||
+		req.query.month == null ||
+		req.query.day == null
+	) {
+		res.status(401).json({
+			message: 'invalid date. must include year, month & day.'
+		});
+	} else {
+		await fetch(
+			'https://calendar.google.com/calendar/ical/daily.schedule@jburroughs.org/public/basic.ics'
+		)
+			.then(response => {
+				return response.text();
+			})
+			.then(data => {
+				let cal = new Calendar(data);
+				res.status(200).json({
+					dayType: cal.getDayType(
+						new Date(
+							req.query.year,
+							req.query.month - 1,
+							req.query.day
+						)
+					)
+				});
+			});
+	}
+}

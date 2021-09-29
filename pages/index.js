e5dcc34874f70ca0fa4cf909fa2db44bb76a2788
Eli Front <elifront23@gmail.com>
2021-09-29 12:34:17 -0500

    period

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,37 @@
 import Layout from '../components/Layout.js';
 import Calendar from '../lib/calendar.js';
+import { SCHEDULE_DAY_TYPES } from '../lib/schedule.js';
 
-export default function Home({ dayType }) {
+export default function Home({ calData }) {
+	let cal = new Calendar(calData);
+
+	const dayType = cal.getDayType(new Date());
+
 	return (
 		<Layout>
 			<h1 className="text-4xl font-medium font-serif mt-5">
-				Day Type: {dayType}
+				Day Type: {SCHEDULE_DAY_TYPES[dayType]}
 			</h1>
+			<h2>
+				Period: {cal.getPeriod(cal.formatTime(new Date()), dayType)}
+			</h2>
 		</Layout>
 	);
 }
 
 export async function getStaticProps({ params }) {
-	const dayType = await fetch(
+	const calData = await fetch(
 		'https://calendar.google.com/calendar/ical/daily.schedule@jburroughs.org/public/basic.ics'
 	)
 		.then(response => {
 			return response.text();
 		})
 		.then(data => {
-			const cal = new Calendar(data);
-
-			return cal.getDayType(new Date());
+			return data;
 		});
 	return {
 		props: {
-			dayType
+			calData
 		}
 	};
 }

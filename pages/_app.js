e83f474514efd0bdc2fd41b7dab9d36c37be83d8
Eli Front <elifront23@gmail.com>
2021-10-13 12:18:40 -0500

    tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/global.css';
+import TagManager from 'react-gtm-module';
+import { useEffect } from 'react';
+
+const tagManagerArgs = {
+	gtmId: 'GTM-K22SFTT'
+};
 
 function MyApp({ Component, pageProps }) {
+	useEffect(() => {
+		TagManager.initialize(tagManagerArgs);
+	}, []);
+
 	return <Component {...pageProps} />;
 }
 

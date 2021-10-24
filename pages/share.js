b9259a537c0674ae2ecb1dedb71d511039ed4d8d
eli-front <elifront23@gmail.com>
2021-10-24 17:52:14 -0500

    layout updates

--- pages/share.js
+++ pages/share.js
@@ -4,7 +4,7 @@ export default function Share() {
 	return (
 		<Layout>
 			<div
-				className="max-h-screen	top-20 absolute bg-center p-64 w-screen bg-no-repeat bg-contain"
+				className="max-w-screen max-h-screen	top-20 absolute bg-center p-64 w-screen bg-no-repeat bg-contain"
 				style={{ backgroundImage: 'url("/qr-code.png")' }}
 			></div>
 		</Layout>

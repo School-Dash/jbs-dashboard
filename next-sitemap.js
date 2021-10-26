6c043d3dde9f09f4fa43f23b199271f63734a99d
Eli Front <elifront23@gmail.com>
2021-10-26 18:34:19 -0500

    sitemap update

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-	siteUrl: process.env.SITE_URL || 'https://elifront.com',
+	siteUrl: process.env.SITE_URL || 'https://jbsdash.netlify.app',
 	generateRobotsTxt: true // (optional)
 	// ...other options
 };

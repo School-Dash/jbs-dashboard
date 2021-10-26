2f765db8bdefacd42095b4c566c52c8d902996ba
Eli Front <elifront23@gmail.com>
2021-10-26 18:41:41 -0500

    404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout.js';
+
+export default function custom404() {
+	return (
+		<Layout>
+			<h1 className="relative top-40 text-center text-4xl font-medium p-5 bg-opacity-50 bg-white w-64 m-auto rounded-lg">
+				Page Not Found
+			</h1>
+		</Layout>
+	);
+}

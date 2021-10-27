2660a4ba52e2281aacda4340d65e58432a818bba
Eli Front <elifront23@gmail.com>
2021-10-26 19:07:43 -0500

    share background

--- pages/share.js
+++ pages/share.js
@@ -4,7 +4,7 @@ export default function Share() {
 	return (
 		<Layout>
 			<div
-				className="w-1/2 h-1/2 max-w-screen max-h-screen top-20 absolute bg-center w-screen bg-no-repeat bg-contain"
+				className="h-80 max-w-xs top-32 absolute left-1/2 transform -translate-x-1/2 bg-center w-2/3 bg-no-repeat bg-contain bg-opacity-50 bg-white rounded-lg"
 				style={{ backgroundImage: 'url("/qr-code.png")' }}
 			></div>
 		</Layout>

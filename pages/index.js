fa9dd778c86ed2b1847ba461d47f7965a76fbc9f
Eli Front <elifront23@gmail.com>
2021-10-27 10:28:42 -0500

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home({ calData }) {
 					{SCHEDULE_DAY_TYPES[dayType]}
 				</h1>
 				{dayType != 'NO_SCHOOL' ? (
-					period == null ? (
+					period != null && !cal.isEndOfDay(period, dayType) ? (
 						<>
 							<h2 className="text-center text-3xl p-4">
 								{cal.formatPeriod(period, dayType)}

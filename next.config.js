ac82db3f876574cd6e61d55e268dd36c5277aa91
Eli Front <elifront23@gmail.com>
2022-01-20 15:25:21 -0600

    redirect fix final try

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 module.exports = {
 	reactStrictMode: true,
 	async redirects() {
-		return process.env.NODE_ENV != 'development'
-			? [
-					{
-						source: '/',
-						destination: 'https://jbsdash.com/',
-						permanent: true
-					}
-			  ]
-			: [];
+		return [
+			{
+				source: '/',
+				destination: 'https://jbsdash.com/',
+				permanent: true
+			}
+		];
 	}
 };

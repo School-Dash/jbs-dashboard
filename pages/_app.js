100200628fe5fb0e965a5b9c5464f0b0538c75de
eli-front <elifront23@gmail.com>
2022-03-10 18:25:20 -0600

    media.net ad

--- pages/_app.js
+++ pages/_app.js
@@ -24,6 +24,16 @@ function MyApp({ Component, pageProps }) {
 				}}
 				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3740922836819487"
 			/>
+			<Script id="MediaAdScript">
+				{`
+				window._mNHandle = window._mNHandle || {};
+				window._mNHandle.queue = window._mNHandle.queue || [];
+				medianet_versionId = "3121199";`}
+			</Script>
+			<Script
+				src="https://contextual.media.net/dmedianet.js?cid=8CU87OT16"
+				async="async"
+			></Script>
 			<Component {...pageProps} />
 		</>
 	);
